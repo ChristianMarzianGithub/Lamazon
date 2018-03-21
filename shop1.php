b8ace395284f9ae0b0eacb4b0ad038eeb190610f
randwerArbeit <marzian@innosoft.de>
2018-03-21 15:16:08 +0100

    Dateien, die ich in der Berufsschule bearbeitet habe

--- shop1.php
+++ shop1.php
@@ -1,63 +1,36 @@
-
-
-<html>
-
-<head>
-
-	<title>Lamazon - Worlds No.1 Online-Shop</title>	
-
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-
-	<link rel="stylesheet" href="style.css">
-
-</head>
-
-
-
-<body id="globalBody">
-
-	  <?php
-
-		session_start();
-
-		include_once('hilfs_funktionen.php');
-
-		$dbh = db_connect("marzian_ws");
-
-		echo "<div class='wrapperLogin'>";
-
-			echo "<div class='Aligner'></div>";
-
-			echo "<div class='Aligner'>
-
-					<a href='index.html'>
-
-						<img src='Unbenannt.png' alt='im Shop anmelden'>
-
-					</a>
-
-					</div>";							
-
-			echo "<div class='Aligner'>";
-
-				echo "<form action='shop2.php' method='POST'>";
-
-					echo "<input type='submit' value='zum Warenkorb hinzuf&uuml;gen'></input>";
-
-					echo db_show_query("select * from produkt",$dbh);
-
-				echo "</form>";
-
-			echo "</div>";
-
-		echo "</div>";
-
-		db_close($dbh);
-	  ?>
-	  <br><a href='warenkorbAnzeigen.php'> Warenkorb anzeigen</a>
-
-</body>
-
+
+<html>
+<head>
+	<title>Lamazon - Worlds No.1 Online-Shop</title>	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body id="globalBody">
+	  <?php
+		session_start();
+		include_once('hilfs_funktionen.php');
+		$dbh = db_connect("marzian_ws");
+		echo "<div class='wrapperLogin'>";
+			echo "<div class='Aligner'></div>";
+			echo "<div class='Aligner'>
+					<a href='index.html'>
+						<img src='Unbenannt.png' alt='im Shop anmelden'>
+					</a>
+					</div>";							
+			echo "<div class='Aligner'>";
+				echo "<form action='shop2.php' method='POST'>";
+					echo "<input type='submit' value='Warenkorb hinzufügen'></input>";
+					echo db_show_query("select * from produkt",$dbh);
+				echo "</form>";
+			echo "</div>";
+		echo "</div>";
+		db_close($dbh);
+		
+		$_SESSION['var'] = "af";
+		$var = $_SESSION['var'];
+		echo $var;
+	  ?>
+</body>
 </html>
--- warenkorbAnzeigen.php
+++ warenkorbAnzeigen.php
@@ -1,45 +1,18 @@
-<html>
-
-<head>
-
-	<title>Lamazon - Worlds No.1 Online-Shop</title>	
-
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-
-	<link rel="stylesheet" href="style.css">
-
-</head>
-
-
-<body id="globalBody">
-
-	  <?php
-
-		include_once('hilfs_funktionen.php');
-
-		$dbh = db_connect("marzian_ws");
-
-		session_start();
-
-		if(isset($_SESSION['warenkorb']))
-		{
-			$var = $_SESSION['warenkorb'];
-		}		
-
-		if(isset($var))
-		{
-			echo gibProdukteArrayAlsTabelleAus($var);	
-		}
-		else
-		{
-			echo "keine Produkte im Warenkorb";
-		}
-		
-
-	  ?>
-	  <br><a href='shop1.php'> zur Produktauswahl</a>
-</body>
-
+<html>
+<head>
+	<title>Lamazon - Worlds No.1 Online-Shop</title>	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body id="globalBody">
+	  <?php
+		include_once('hilfs_funktionen.php');
+		$dbh = db_connect("marzian_ws");
+		session_start();
+		$var = $_SESSION['warenkorb'];
+		echo gibProdukteArrayAlsTabelleAus($var);	
+	  ?>
+</body>
 </html>
