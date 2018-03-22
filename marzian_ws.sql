ba8bfc5786b6ee8f8c8b3edef44dd92dae2b582c
randwerArbeit <marzian@innosoft.de>
2018-03-22 13:46:20 +0100

    Kategorie zu produkt-tabelle hinzugefügt

--- marzian_ws.sql
+++ marzian_ws.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 22. Mrz 2018 um 12:17
+-- Erstellungszeit: 22. Mrz 2018 um 13:45
 -- Server-Version: 10.1.30-MariaDB
 -- PHP-Version: 7.2.1
 
@@ -65,7 +65,18 @@ INSERT INTO `bestellung` (`BID`, `KundeID`, `ArtNr`, `Anzahl`) VALUES
 (1, 1, 5, 1),
 (1, 1, 6, 1),
 (1, 1, 10, 1),
-(1, 1, 11, 1);
+(1, 1, 11, 1),
+(2, 1, 1, 1),
+(2, 1, 2, 1),
+(2, 1, 3, 1),
+(3, 1, 2, 1),
+(3, 1, 3, 1),
+(4, 1, 8, 1),
+(5, 1, 3, 1),
+(5, 1, 4, 1),
+(6, 2, 3, 1),
+(6, 2, 4, 1),
+(7, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -134,7 +145,8 @@ CREATE TABLE `kunde` (
 --
 
 INSERT INTO `kunde` (`Kid`, `Kennung`, `Vorname`, `Nachname`, `Strasse`, `PLZ`, `Ort`, `KontoNummer`, `BLZ`, `BankInstitut`, `Passwort`) VALUES
-(1, 'g', '', '', '', '', '', '', '', '', 'h');
+(1, 'g', '', '', '', '', '', '', '', '', 'h'),
+(2, 'asdf', '', '', '', '', '', '', '', '', 'asdf');
 
 -- --------------------------------------------------------
 
@@ -146,26 +158,27 @@ CREATE TABLE `produkt` (
   `ArtNr` int(11) NOT NULL,
   `Name` text NOT NULL,
   `Beschreibung` text NOT NULL,
-  `Preis` decimal(10,0) NOT NULL
+  `Preis` decimal(10,0) NOT NULL,
+  `KatID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `produkt`
 --
 
-INSERT INTO `produkt` (`ArtNr`, `Name`, `Beschreibung`, `Preis`) VALUES
-(1, 'HP ScanJet3300C', 'Flachbettscanner', '99'),
-(2, 'HP ScanJet 2220A', 'Flachbettscanner', '56'),
-(3, 'HP LaseerJet 3477C', 'Laserdrucker', '299'),
-(4, 'HP LaserJet 7769 C', 'Farblaserdrucker', '1590'),
-(5, 'MD 1772 JC', 'Monitor', '150'),
-(6, 'MD 6155 AH', '17 Zoll TFT Bildschirm', '350'),
-(7, 'MD 1334 S', 'Flachbettscanner', '65'),
-(8, 'MD 2443 S', 'Flachbettscanner', '76'),
-(9, 'SI 1221 C', '19 Zoll Monitor', '200'),
-(10, 'SI 7822 TFT', '21 Zoll TFT-Monitor', '569'),
-(11, 'SI P 3244', 'DIN A1 Plotter', '1590'),
-(12, 'SI D 1121 C', 'Farblaserdrucker', '547');
+INSERT INTO `produkt` (`ArtNr`, `Name`, `Beschreibung`, `Preis`, `KatID`) VALUES
+(1, 'HP ScanJet3300C', 'Flachbettscanner', '99', 1),
+(2, 'HP ScanJet 2220A', 'Flachbettscanner', '56', 1),
+(3, 'HP LaseerJet 3477C', 'Laserdrucker', '299', 3),
+(4, 'HP LaserJet 7769 C', 'Farblaserdrucker', '1590', 3),
+(5, 'MD 1772 JC', 'Monitor', '150', 2),
+(6, 'MD 6155 AH', '17 Zoll TFT Bildschirm', '350', 2),
+(7, 'MD 1334 S', 'Flachbettscanner', '65', 1),
+(8, 'MD 2443 S', 'Flachbettscanner', '76', 1),
+(9, 'SI 1221 C', '19 Zoll Monitor', '200', 2),
+(10, 'SI 7822 TFT', '21 Zoll TFT-Monitor', '569', 2),
+(11, 'SI P 3244', 'DIN A1 Plotter', '1590', 3),
+(12, 'SI D 1121 C', 'Farblaserdrucker', '547', 1);
 
 -- --------------------------------------------------------
 
@@ -188,7 +201,14 @@ INSERT INTO `rechnung` (`RID`, `BID`, `Datum`, `UhrzeitErstellung`) VALUES
 (1, 1, '0000-00-00', '0000-00-00 00:00:00'),
 (2, 1, '0000-00-00', '0000-00-00 00:00:00'),
 (3, 1, '0000-00-00', '0000-00-00 00:00:00'),
-(4, 1, '0000-00-00', '0000-00-00 00:00:00');
+(4, 1, '0000-00-00', '0000-00-00 00:00:00'),
+(5, 2, '0000-00-00', '0000-00-00 00:00:00'),
+(6, 3, '0000-00-00', '0000-00-00 00:00:00'),
+(7, 4, '0000-00-00', '0000-00-00 00:00:00'),
+(8, 5, '0000-00-00', '0000-00-00 00:00:00'),
+(9, 6, '0000-00-00', '0000-00-00 00:00:00'),
+(10, 6, '0000-00-00', '0000-00-00 00:00:00'),
+(11, 7, '0000-00-00', '0000-00-00 00:00:00');
 
 --
 -- Indizes der exportierten Tabellen
@@ -262,7 +282,7 @@ ALTER TABLE `kategorie`
 -- AUTO_INCREMENT für Tabelle `kunde`
 --
 ALTER TABLE `kunde`
-  MODIFY `Kid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Kid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT für Tabelle `produkt`
@@ -274,7 +294,7 @@ ALTER TABLE `produkt`
 -- AUTO_INCREMENT für Tabelle `rechnung`
 --
 ALTER TABLE `rechnung`
-  MODIFY `RID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `RID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
